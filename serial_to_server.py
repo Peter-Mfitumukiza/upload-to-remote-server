f83773eccdd18db6c996a3cbb5cbbab3afe336b3
Mfitumukiza Peter <mfitep6@gmail.com>
2022-06-20 06:31:46 +0200

    added python

---
+++ serial_to_server.py
@@ -0,0 +1,29 @@
+import serial
+import requests
+import os
+
+while True: 
+    if os.path.exists("transactions.txt") :
+        file_data = ''
+        transactions_file = open('transactions.txt')
+        while True:
+            line = transactions_file.readline()
+            if not line: 
+                break
+            file_data += line + "\n"  
+
+        formData = {"data": file_data}
+        response = requests.post("https://transactions-server.herokuapp.com/transactions/new", data=formData)
+
+        if response.status_code == 200 :
+            print("Data uploaded!")
+
+        os.remove("transactions.txt");
+        ser = serial.Serial('COM3', 9600)
+        ser.write(b'Data Uploaded')
+
+       
+        time.sleep(20)
+    
+    else :
+        time.sleep(20)
